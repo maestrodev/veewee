d9eb1861978e8cb5b01d90a67668d96cb7c5cf1e
Carlos Sanchez <csanchez@maestrodev.com>
2012-04-06 15:47:18 +0200

    Fix epel escaping

--- templates/CentOS-6.2-x86_64-openjdk/postinstall.sh
+++ templates/CentOS-6.2-x86_64-openjdk/postinstall.sh
@@ -13,7 +13,7 @@ EOM
 cat > /etc/yum.repos.d/epel.repo << EOM
 [epel]
 name=epel
-mirrorlist=http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch
+mirrorlist=http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=\$basearch
 enabled=1
 gpgcheck=0
 EOM
